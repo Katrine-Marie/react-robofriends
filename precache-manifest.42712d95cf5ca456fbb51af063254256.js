ea0f94e075801d0edc28c7db737585010f46f53e
Katrine-Marie Burmeister <fjordscene@gmail.com>
2020-01-13 21:25:35 +0100

    Updates

---
+++ precache-manifest.42712d95cf5ca456fbb51af063254256.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be1a0f6c9dd58a12fc23209400140c56",
+    "url": "/react-robofriends/index.html"
+  },
+  {
+    "revision": "db030a0313823e763c87",
+    "url": "/react-robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "989b49f9130c2165a9e0",
+    "url": "/react-robofriends/static/css/main.19f65978.chunk.css"
+  },
+  {
+    "revision": "db030a0313823e763c87",
+    "url": "/react-robofriends/static/js/2.fbad7c07.chunk.js"
+  },
+  {
+    "revision": "989b49f9130c2165a9e0",
+    "url": "/react-robofriends/static/js/main.8f6c6f93.chunk.js"
+  },
+  {
+    "revision": "432bfcdc022c90d2629e",
+    "url": "/react-robofriends/static/js/runtime-main.5df72122.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/react-robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
